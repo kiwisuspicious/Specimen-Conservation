be9f8d471abe54c4f453e8a3d885115b8be77f76
Muhd Faiq Mohammad <muhammadfaiqmohammad@gmail.com>
2024-12-20 02:01:32 +0800

    1

--- condition_report.sql
+++ condition_report.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2024 at 02:23 PM
+-- Generation Time: Dec 19, 2024 at 06:57 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `adminuser`
+--
+
+CREATE TABLE `adminuser` (
+  `id` int(11) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `adminuser`
+--
+
+INSERT INTO `adminuser` (`id`, `email`, `password`) VALUES
+(1, 'faiq2001@gmail.com', '$2y$10$RirvofsKI.4eitSmwf0KT.9SMpYqZ/IEJCc6fvD1yIrqrrKfFF/DO');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `application`
 --
 
@@ -38,26 +57,43 @@ CREATE TABLE `application` (
   `workmeth` text NOT NULL,
   `inspectname` varchar(255) NOT NULL,
   `remarks` text NOT NULL,
-  `appID` varchar(8) NOT NULL
+  `appID` varchar(8) NOT NULL,
+  `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `application`
 --
 
-INSERT INTO `application` (`email`, `catnum`, `specname`, `location`, `examination`, `speccond`, `material`, `workmeth`, `inspectname`, `remarks`, `appID`) VALUES
-('test@example.com', 'dasd', 'asdasd', 'Natural History Building', 'Dirt Accumulation (Pengumpulan kotoran)', 2, 'Japanese Tissue, Wood Clay', 'dsadsad', 'Jane Smith', 'dsadas', 'APR72830'),
-('test@example.com', 'test', 'test', 'Natural History Building', 'Dirt Accumulation (Pengumpulan kotoran), Stitch opening (Jahitan terbuka)', 1, 'Japanese Tissue, Wood Clay', 'test', 'John Doe', 'test', 'APR80630');
+INSERT INTO `application` (`email`, `catnum`, `specname`, `location`, `examination`, `speccond`, `material`, `workmeth`, `inspectname`, `remarks`, `appID`, `status`) VALUES
+('test@example.com', 'dasd', 'asdasd', 'Natural History Building', 'Dirt Accumulation (Pengumpulan kotoran)', 2, 'Japanese Tissue, Wood Clay', 'dsadsad', 'Jane Smith', 'dsadas', 'APR72830', 2),
+('test@example.com', 'test', 'test', 'Natural History Building', 'Dirt Accumulation (Pengumpulan kotoran), Stitch opening (Jahitan terbuka)', 1, 'Japanese Tissue, Wood Clay', 'test', 'John Doe', 'test', 'APR80630', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `adminuser`
+--
+ALTER TABLE `adminuser`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `application`
 --
 ALTER TABLE `application`
   ADD PRIMARY KEY (`appID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `adminuser`
+--
+ALTER TABLE `adminuser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
